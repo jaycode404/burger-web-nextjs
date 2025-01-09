0df5d866e39ca9c9b34fc1c18acb750f5f43bb86
FLRSscript <flrsscript@users.noreply.github.com>
2025-01-09 17:26:40 -0600

    fixed burguer on desktop version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -184,7 +184,7 @@ export default function Page() {
                 type: "spring",
                 stiffness: 100
               }}
-              className="relative w-[85%] md:w-[45%] lg:absolute lg:top-[15%] lg:-translate-y-1/2 lg:right-0 lg:w-[45%] max-w-xl md:max-w-lg lg:max-w-2xl pt-24 md:pt-16 lg:pt-8"
+              className="relative w-[85%] md:w-[45%] lg:absolute lg:top-[27%] lg:-translate-y-1/2 lg:right-0 lg:w-[45%] max-w-xl md:max-w-lg lg:max-w-3xl pt-24 md:pt-16 lg:pt-8"
             >
               <div className="relative md:w-[100%]">
                 <div className="absolute inset-0 bg-gradient-radial from-[#FFE600] via-[#FFE600]/30 to-transparent opacity-60 blur-[50px] md:blur-[80px] lg:blur-[100px] rounded-full transform scale-100 md:scale-125 lg:scale-150"></div>
